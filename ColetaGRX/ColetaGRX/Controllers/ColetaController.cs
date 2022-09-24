b26c50ba1b81de723ead778a65238423ed084d16
vitorbda <joaovitorveloz@gmail.com>
2022-09-24 18:27:39 -0300

    Criação do controller da coleta

---
+++ ColetaGRX/ColetaGRX/Controllers/ColetaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ColetaGRX.Controllers
+{
+    public class ColetaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
