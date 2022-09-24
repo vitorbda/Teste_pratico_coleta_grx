3087d2b376cde1152e8a2c1a0292f110e4fcca8e
vitorbda <joaovitorveloz@gmail.com>
2022-09-24 19:12:18 -0300

    Função que verifica se a textbox está correta

--- ColetaGRX/ColetaGRX/wwwroot/js/coleta.js
+++ ColetaGRX/ColetaGRX/wwwroot/js/coleta.js
@@ -11,3 +11,17 @@ function addItem(valor) {
     novoItem.value = "";
     localStorage.meuArr = JSON.stringify(arr);
 }
+
+let inputs = document.querySelector("textarea")
+let button = document.getElementById("botaoSalvar")
+button.disabled = true;
+
+inputs.addEventListener('input', () => {
+    const min = 15
+    const max = 200
+    if (inputs.value.length > max || inputs.value.length < min) {
+        button.disabled = true
+    } else {
+        button.disabled = false
+    }
+})
