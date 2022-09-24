c0ceb6bfab300ee0ed6b422d6e809aa774462089
vitorbda <joaovitorveloz@gmail.com>
2022-09-24 19:09:49 -0300

    Criação da função de salvar os dados na LocalStorage

--- ColetaGRX/ColetaGRX/wwwroot/js/coleta.js
+++ ColetaGRX/ColetaGRX/wwwroot/js/coleta.js
@@ -1,1 +1,13 @@
-﻿
+﻿var arr = [];
+function addItem(valor) {
+    if (localStorage) {
+        arr = JSON.parse(localStorage.getItem('meuArr'));
+    }
+
+    let novoItem = valor;
+    console.log(novoItem)
+    this.arr = this.arr || [];
+    this.arr.push(novoItem)
+    novoItem.value = "";
+    localStorage.meuArr = JSON.stringify(arr);
+}
